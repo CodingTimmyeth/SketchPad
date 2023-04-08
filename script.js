4ca5c7e0f4a162945f15b5b8fd01886e34f81e07
Tommy Miguel <tommymiguel@Tommys-MacBook-Pro.local>
2023-04-08 15:37:48 -0700

    Project Finsihed

--- script.js
+++ script.js
@@ -1,28 +1,64 @@
-// Variables for the Color Configuration
-const container = document.getElementById('container')
-const eraserBtn = document.getElementById('Eraser')
-const blackBtn = document.getElementById('black')
+const container = document.getElementById('container');
+const screenDiv = document.querySelector('.screen');
+const userInput = prompt('Enter the number of divs you want to create:');
+const eraserBtn = document.getElementById('Eraser');
+const blackBtn = document.getElementById('black');
+const redBtn = document.getElementById('red');
+const newGrid = document.getElementById('new-grid')
 
-for(i = 0; i < 64; i++){
-    const newDiv = document.createElement('div')
-    newDiv.classList.add('cubics')
-    container.append(newDiv)
 
-    newDiv.addEventListener('mouseover', () => {
-        newDiv.style.backgroundColor = 'red'
-    })
-
-    blackBtn.addEventListener('click', () => {
-        newDiv.addEventListener('mouseover', () => {
-            newDiv.style.backgroundColor = 'black'
-        })
-    })
-
-    eraserBtn.addEventListener('click', () => {
-        newDiv.addEventListener('mouseover', () => {
-            newDiv.style.backgroundColor = 'white'
-        })
-    })
+function createGrid(numSquares) {
+  const totalSquares = numSquares * numSquares;
+  container.style.gridTemplateColumns = `repeat(${numSquares}, 1fr)`;
+  container.style.gridTemplateRows = `repeat(${numSquares}, 1fr)`;
+  for (let i = 0; i < totalSquares; i++) {
+    const gridDiv = document.createElement('div');
+    gridDiv.classList.add('cubics');
+    container.appendChild(gridDiv);
+  }
 }
 
-// Variables for the layout grid
+createGrid(16);
+
+newGrid.addEventListener('click', () => {
+  // Prompt the user to enter the number of squares they want for the new grid
+  const userInput = prompt('Enter the number of squares you want for the new grid:');
+  // Clear the existing grid
+  container.innerHTML = '';
+  // Generate a new grid based on the user's input
+  createGrid(userInput);
+});
+
+
+for (let i = 0; i < userInput; i++) {
+  const newDiv = document.createElement('div');
+  newDiv.classList.add('cubics');
+  container.append(newDiv);
+}
+
+redBtn.addEventListener('click', () => {
+  const divs = document.querySelectorAll('.cubics');
+  divs.forEach((div) => {
+    div.addEventListener('mouseover', () => {
+      div.style.backgroundColor = 'red';
+    });
+  });
+});
+
+blackBtn.addEventListener('click', () => {
+  const divs = document.querySelectorAll('.cubics');
+  divs.forEach((div) => {
+    div.addEventListener('mouseover', () => {
+      div.style.backgroundColor = 'black';
+    });
+  });
+});
+
+eraserBtn.addEventListener('click', () => {
+  const divs = document.querySelectorAll('.cubics');
+  divs.forEach((div) => {
+    div.addEventListener('mouseover', () => {
+      div.style.backgroundColor = 'white';
+    });
+  });
+});
